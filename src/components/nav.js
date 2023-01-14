59dc73c898894a016c0a70b28d57529c9f6ad07f
Gilbert Malova <malovagilbert@gmail.com>
2023-01-14 05:19:39 +0200

    Added navbar component and styling

---
+++ src/components/nav.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import '../styles/nav.css';
+
+const Nav = () => {
+  const [navItems] = useState(['HOME', 'ABOUT', 'PROJECTS', 'CONTACT']);
+
+  const navList = navItems.map((navItem) => 
+    <li>{navItem}</li>
+  )
+
+
+  return (
+    <nav>
+      <div>Malova</div>
+      <ul className="navItems">{navList}</ul>
+    </nav>
+  )
+}
+
+export default Nav;
