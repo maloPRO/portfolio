ac86587aa2fe4657fa7e81eb6b09b17fca6c412c
Gilbert Malova <malovagilbert@gmail.com>
2023-01-19 16:02:27 +0200

    Added contacts card

--- src/components/contacts.jsx
+++ src/components/contacts.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import '../styles/contacts.css';
 
 const Contacts = () => {
 
   return (
     <div className="contacts">
-
+      <h1>Contacts</h1>
+      <div className="contactsCard">
+        <div></div>
+        <div></div>
+      </div>
     </div>
   )
 };
