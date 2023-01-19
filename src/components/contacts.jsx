962331c5ebcdc5206d277d107ff904f3da5acb27
Gilbert Malova <malovagilbert@gmail.com>
2023-01-19 17:50:00 +0200

    added input form

--- src/components/contacts.jsx
+++ src/components/contacts.jsx
@@ -7,7 +7,25 @@ const Contacts = () => {
     <div className="contacts">
       <h1>Contacts</h1>
       <div className="contactsCard">
-        <div></div>
+        <div className="cardLeft">
+          <h2>Send Me a Message</h2>
+          <form>
+            <label>
+              <input type='text' name='name' placeholder='Your Name'></input>
+            </label>
+            <label>
+              <input type='email' name='email' placeholder='Your Email'></input>
+            </label>
+            <label>
+              <input type='text' name='subject' placeholder='Subject'></input>
+            </label>
+            <label>
+              <textarea type='text' rows='8' name='message' placeholder='Message'></textarea>
+            </label>
+            <button>Submit</button>
+            
+          </form>
+        </div>
         <div></div>
       </div>
     </div>
