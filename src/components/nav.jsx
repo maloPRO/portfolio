2c88b8d3fe99a7f555a24dae22caaf423a4e9cd8
Gilbert Malova <malovagilbert@gmail.com>
2023-05-05 08:11:11 +0200

    Added mobile menu

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from "react";
 import uniqid from 'uniqid';
 
 const Nav = () =>  {
@@ -23,17 +24,27 @@ const Nav = () =>  {
     ]);
 
     const navItems = items.map((navItem) =>
-        <li className="ml-4 text-lg font-bold" key={navItem.id}>
-            <a>{navItem.name}</a>
-        </li>
+    <li key={navItem.id}>
+        <a href="#" className="block py-2 pl-3 pr-4 bg-blue-700 text-white rounded md:bg-transparent md:p-0" aria-current="page">{navItem.name}</a>
+    </li>
     )
 
     return (
-        <div className="grid grid-cols-2 w-screen h-20 lg:grid-cols-3 ">
-            <div className="flex items-center pl-4 lg:pl-36 text-white text-3xl font-bold">Malova</div>
-            <div className="lg:col-span-2 pr-36">
-                <ul className="hidden lg:flex lg:h-full lg:items-center lg:justify-end text-white">{navItems}</ul>
-            </div>
+        <div>
+            <nav>
+                <div className="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <a href="#" className="flex items-center">
+                        <span className="self-center text-4xl text-white font-bold whitespace-nowrap ">Malova</span>
+                    </a>
+                    <button data-collapse-toggle="navbar-solid-bg" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-solid-bg" aria-expanded="false">
+                        <span className="sr-only">Open main menu</span>
+                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                    </button>
+                    <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+                        <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent">{navItems}</ul>
+                    </div>
+                </div>
+            </nav>
         </div>
     )
 }
