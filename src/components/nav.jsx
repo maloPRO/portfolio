950b38563e1c0ded47d9ef3248aef78040d10e7d
Gilbert Malova <malovagilbert@gmail.com>
2023-05-05 08:48:12 +0200

    Mobile menu

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -23,6 +23,13 @@ const Nav = () =>  {
         }
     ]);
 
+    const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuVisible(!isMenuVisible);
+
+    }
+
     const navItems = items.map((navItem) =>
     <li key={navItem.id}>
         <a href="#" className="block py-2 pl-3 pr-4 bg-blue-700 text-white rounded md:bg-transparent md:p-0" aria-current="page">{navItem.name}</a>
@@ -36,11 +43,33 @@ const Nav = () =>  {
                     <a href="#" className="flex items-center">
                         <span className="self-center text-4xl text-white font-bold whitespace-nowrap ">Malova</span>
                     </a>
-                    <button data-collapse-toggle="navbar-solid-bg" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-solid-bg" aria-expanded="false">
+                    <button 
+                        data-collapse-toggle="navbar-solid-bg" 
+                        type="button" 
+                        className="inline-flex items-center p-2 ml-3 text-sm text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" 
+                        aria-controls="navbar-solid-bg" 
+                        aria-expanded="false"
+                        onClick={toggleMenu}
+                    >
                         <span className="sr-only">Open main menu</span>
-                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                        <svg 
+                            className="w-6 h-6" 
+                            aria-hidden="true" 
+                            fill="currentColor" 
+                            viewBox="0 0 20 20" 
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fill-rule="evenodd" 
+                                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" 
+                                clip-rule="evenodd"
+                            ></path>
+                        </svg>
                     </button>
-                    <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+                    <div 
+                        className={`${isMenuVisible ? "block" : "hidden"} w-full md:block md:w-auto`} 
+                        id="navbar-solid-bg"
+                    >
                         <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent">{navItems}</ul>
                     </div>
                 </div>
